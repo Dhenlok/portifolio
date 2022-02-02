cffa1479596df8d2814bbe5f4288975a1d07e61c
Dhenlok <lhsabino18@gmail.com>
2022-02-02 14:58:13 -0300

    portifólio incompleto

---
+++ portifolio/src/components/Header/Header.js
@@ -0,0 +1,12 @@
+import { ContainerHeader } from "./styled";
+
+const Header = () => {
+  return (
+    <ContainerHeader>
+      <p>Lucas</p>
+      <p>Bem-vindo(a)</p>
+      <a>Contato</a>
+    </ContainerHeader>
+  );
+};
+export default Header
